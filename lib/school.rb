bd1aa436d274e2e23fb064411aa6131e3ead58ed
Patrick Kim <pkim050@gmail.com>
2019-02-19 08:18:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @hash = {}
+  end
+  
+  def roster
+    @hash
+  end
+  
+  def add_student(name, grade)
+    #binding.pry
+    counter = 0
+    #@hash.each {|k, v| counter += 1 if k == grade}
+    @hash[grade] = [] if @hash.key?(grade) == false
+    #@hash[grade] = [] if counter == 0
+    @hash[grade] << name
+  end
+  
+  def grade(grade)
+    @hash[grade]
+  end
+  
+  def sort
+    @hash.each {|k, v| @hash[k] = v.sort}
+    @hash.sort_by {|k, v| k}.to_h
+  end
+end
